3773f16fe03b854ba78d17662488947317c1406d
Andrés Miguélez <andres.miguelez2@gmail.com>
2026-01-03 19:25:24 +0100

    feat: :sparkles: Seleccionar clasificación por jornada

--- app/server_requests/leaderboard.py
+++ app/server_requests/leaderboard.py
@@ -6,33 +6,63 @@ from .logger import logger
 router = APIRouter(prefix="/leaderboard", tags=["leaderboard"])
 
 
-@router.get('')
-def leaderboard():
+@router.get('/{fixture_id}')
+def leaderboard(fixture_id: str):
     """Get the leaderboard of players
     """
     try:
         conn = pg_connect()
 
         cursor = conn.cursor()
-        cursor.execute(
-            """
-            SELECT
-                -- row_number() OVER (ORDER BY points DESC) AS rank
-                player.id
-                , player.name
-                , player.points
-                , f.team_value
-            FROM player
-            LEFT JOIN (
+
+        if fixture_id != "total":
+            cursor.execute(
+                """
                 SELECT
-                    footballer.owner_id
-                    , SUM(footballer_data.value) AS team_value
-                FROM footballer JOIN footballer_data ON footballer.id = footballer_data.id
-                GROUP BY footballer.owner_id
-            ) AS f ON player.id = f.owner_id
-            ORDER BY points DESC
-            """
-        )
+                    -- row_number() OVER (ORDER BY points DESC) AS rank
+                    player.id
+                    , player.name
+                    , player.points
+                    , f.team_value
+                FROM player
+                LEFT JOIN (
+                    SELECT
+                        footballer.owner_id
+                        , SUM(footballer_data.value) AS team_value
+                    FROM footballer JOIN footballer_data ON footballer.id = footballer_data.id
+                    GROUP BY footballer.owner_id
+                ) AS f ON player.id = f.owner_id
+                ORDER BY points DESC
+                """
+            )
+        else:
+            cursor.execute(
+                """
+                SELECT
+                    player.id
+                    , player.name
+                    , fixture.points
+                    , f.team_value
+                FROM player
+                LEFT JOIN (
+                    SELECT
+                        footballer.owner_id
+                        , SUM(footballer_data.value) AS team_value
+                    FROM footballer JOIN footballer_data ON footballer.id = footballer_data.id
+                    GROUP BY footballer.owner_id
+                ) AS f ON player.id = f.owner_id
+                LEFT JOIN (
+                    SELECT
+                        player_id
+                        , points
+                    FROM fixture_details
+                    WHERE fixture_n = %s
+                ) AS fixture ON fixture.player_id = player.id
+                ORDER BY points DESC
+                """,
+                (fixture_id,)
+            )
+        
         players = cursor.fetchall()
 
         cursor.close()
