d374e1db3339d4f50666fb1e35559f1438786394
Andrés Miguélez <andres.miguelez2@gmail.com>
2026-01-18 21:56:10 +0000

    fix: :bug: Cambiar IP

--- app/server_requests/server_requests.py
+++ app/server_requests/server_requests.py
@@ -18,7 +18,7 @@ server_app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "http://192.168.1.75:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
