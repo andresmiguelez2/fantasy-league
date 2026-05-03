81576e42b5229c06288b27b01e9501679fd66039
Andrés Miguélez <andres.miguelez2@gmail.com>
2026-05-03 17:34:11 +0200

    fix: :bug: Corregir posición en query

--- app/server_requests/squad.py
+++ app/server_requests/squad.py
@@ -32,15 +32,7 @@ def squad(player_id: int, league_id: int):
                 , fd.position
             FROM footballer f LEFT JOIN footballer_data fd ON f.id = fd.id
             WHERE f.owner_id = %s AND f.league_id = %s
-            ORDER BY
-                CASE LOWER(fd.position)
-                    WHEN 'gk' THEN 1
-                    WHEN 'def' THEN 2
-                    WHEN 'mid' THEN 3
-                    WHEN 'fwd' THEN 4
-                    ELSE 5
-                END,
-                fd.name
+            ORDER BY fd.position, fd.name
             """,
             (player_id, league_id),
         )
